906df52cbf417896a37bc54a7edca68c2380ac82
Muail07 <roots.usama@gmail.com>
2024-05-23 12:31:39 +0500

    GlobalStyles.js

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,26 @@
+// src/styles/GlobalStyles.js
+
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Arial', sans-serif;
+    background-color: #f4f4f4;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+    padding: 0;
+  }
+
+  p {
+    margin: 0;
+  }
+`;
